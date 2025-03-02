2705b9a28774a432fb4624ad50ab906e68190571
Harry M <thenestofficialmail@gmail.com>
2025-03-02 18:21:15 +0000

    Finished projects page w all features (added github section, need to finalise database information)

--- src/components/buttons/SocialButton.tsx
+++ src/components/buttons/SocialButton.tsx
@@ -2,7 +2,10 @@ import {JSXElement} from "solid-js";
 
 export default function SocialButton(props: any): JSXElement {
     return (
-        <a href={props.href} type="button" class="inline-block transition ease-in-out delay-75 hover:-translate-y-2
+        <a href={props.href}
+           target="_blank"
+           type="button"
+           class="inline-block transition ease-in-out delay-75 hover:-translate-y-2
         hover:scale-100 duration-300">
             <div class="me-2">
                 {props.children}
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -23,10 +23,13 @@ export default function Projects(): JSXElement {
     const params: Params = useParams();
     const [project] = createResource(params.name, fetchProject);
     const [scrollActive, setScrollActive] = createSignal(false);
+    const [enlargeType, setEnlargeType] = createSignal(true);
 
     let scrollContainer: HTMLDivElement | undefined;
     let enlargedImageElement: HTMLImageElement | undefined;
     let enlargedImageContainer: HTMLDivElement | undefined;
+    let enlargedIFrameElement: HTMLIFrameElement | undefined;
+    let toggleEnlargeButton: HTMLButtonElement | undefined;
 
     const scrollRight = () => {
         if (scrollContainer) {
@@ -43,12 +46,33 @@ export default function Projects(): JSXElement {
     };
 
     const toggleEnlargeImage = (event: MouseEvent) => {
-        if (enlargedImageContainer && enlargedImageElement) {
+        if (enlargedImageContainer && enlargedImageElement && toggleEnlargeButton) {
+            setEnlargeType(false);
             enlargedImageContainer.classList.toggle("hidden");
             const imageElement: HTMLImageElement = event.currentTarget as HTMLImageElement;
             enlargedImageElement.src = imageElement.src;
+            let iFrameElement: HTMLIFrameElement = ((enlargedImageElement.parentElement as HTMLDivElement).children[2] as HTMLIFrameElement);
+            iFrameElement.classList.toggle("hidden");
+
             if(enlargedImageContainer.classList.contains("hidden")) {
                 setScrollActive(false);
+            } else {
+                setScrollActive(true);
+            }
+        }
+    }
+
+    const toggleEnlargeIFrame = () => {
+        if (enlargedImageContainer && enlargedIFrameElement && toggleEnlargeButton) {
+            setEnlargeType(true);
+            enlargedImageContainer.classList.toggle("hidden");
+            enlargedIFrameElement.src = project()["github_readme"];
+            let imageElement: HTMLImageElement = ((enlargedIFrameElement.parentElement as HTMLDivElement).children[1] as HTMLImageElement);
+            imageElement.classList.toggle("hidden");
+
+            if(enlargedImageContainer.classList.contains("hidden")) {
+                setScrollActive(false);
+
             } else {
                 setScrollActive(true);
             }
@@ -115,47 +139,93 @@ export default function Projects(): JSXElement {
                         </section>
                         <div class={"spacer projectSpacer2"}/>
                         <section class="bg-[#2C3A4A] flex flex-col">
-                            <h1 class="text-4xl font-bold pb-4">Screenshots</h1>
+                            <h1 class="text-4xl font-bold">Screenshots</h1>
+                            <h4 class="pt-1 italic pb-4">Click on an image to enlarge it...</h4>
                             <div class="flex flex-row bg-gray-700 rounded-3xl">
-                                <button type="button" onClick={scrollLeft}>
-                                    <svg width="32" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
-                                         clip-rule="evenodd" fill="white" fill-opacity="0.8">
+                                <button type="button"
+                                        onClick={scrollLeft}
+                                        class="ease-in-out hover:scale-125">
+                                    <svg width="32"
+                                         height="24"
+                                         xmlns="http://www.w3.org/2000/svg"
+                                         fill-rule="evenodd"
+                                         clip-rule="evenodd"
+                                         fill="white"
+                                         fill-opacity="0.8">
                                         <path d="M28 2v4l-12 6 12 6v4l-16-10 16-10z"/>
                                     </svg>
                                 </button>
                                 <div class="p-4 max-w-300">
-                                    <div id="slider" class="flex flex-row overflow-scroll scroll-smooth" ref={scrollContainer}>
+                                    <div id="slider"
+                                         class="flex flex-row overflow-scroll scroll-smooth"
+                                         ref={scrollContainer}>
                                         <For each={project()["slider_images"]}>
                                             {(object) =>
-                                                <img src={object} onClick={toggleEnlargeImage} class="w-140 h-auto p-2"/>}
+                                                <img src={object} onClick={toggleEnlargeImage}
+                                                     class="w-140 h-auto p-2"/>}
                                         </For>
                                     </div>
                                 </div>
-                                <button type="button" onClick={scrollRight}>
-                                    <svg width="32" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
-                                         clip-rule="evenodd" fill="white" fill-opacity="0.8">
+                                <button type="button"
+                                        onClick={scrollRight}
+                                        class="ease-in-out hover:scale-125">
+                                    <svg width="32"
+                                         height="24"
+                                         xmlns="http://www.w3.org/2000/svg"
+                                         fill-rule="evenodd"
+                                         clip-rule="evenodd"
+                                         fill="white"
+                                         fill-opacity="0.8">
                                         <path d="M4 2l16 10-16 10v-4l12-6-12-6V2z"/>
                                     </svg>
                                 </button>
                             </div>
                         </section>
                         <div class={"spacer projectSpacer3"}/>
-                        <section class="bg-[#3B4859]">
-                            <h1 class="text-4xl font-bold">Code</h1>
+                        <section class="bg-[#3B4859] flex flex-col">
+                            <h1 class="text-4xl font-bold">Github</h1>
+                            <h4 class="pt-1 italic">Links to relevant Github repositories...</h4>
+                            <div class="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 p-5">
+                                <a type="button"
+                                   class="border-gray-500 bg-gray-300
+                                 text-black border-solid rounded-xl border-2 font-bold
+                                  text-sm pl-10 pr-10 pt-4 pb-4 opacity-85 mr-2 ease-in-out
+                                  hover:scale-110"
+                                   onClick={toggleEnlargeIFrame}>Open README.md</a>
+                                <a type="button"
+                                   class="border-gray-500 bg-gray-300
+                                 text-black border-solid rounded-xl border-2 font-bold
+                                  text-sm pl-10 pr-10 pt-4 pb-4 opacity-85 ml-2 ease-in-out
+                                  hover:scale-110"
+                                   href={project()["github_project"]}
+                                   target="_blank">Open Project</a>
+                            </div>
                         </section>
                         <div class="bg-[#3B4859]">
-                            <br/>
+                        <br/>
                             <SocialButtonGroup inverted="true"/>
                             <br/>
                             <br/>
                         </div>
                     </div>
                 </div>
-                <div ref={enlargedImageContainer} class="fixed w-full h-full left-0 top-0 grid place-items-center hidden">
+                <div ref={enlargedImageContainer}
+                     class="fixed w-full h-full left-0 top-0 grid place-items-center hidden">
                     <div class="absolute inset-0 backdrop-blur-lg"/>
-                    <img src={""} ref={enlargedImageElement} class="w-210 h-auto relative z-10"/>
-                    <button class="absolute top-0 right-0 p-5" onClick={toggleEnlargeImage}>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="35" height="35" fill="white"
+                    <img src={""}
+                         ref={enlargedImageElement}
+                         class="w-250 h-auto relative z-10"/>
+                    <iframe src={""} ref={enlargedIFrameElement}
+                            class="absolute w-180 min-w-100 h-200 min-h-100"></iframe>
+                    <button ref={toggleEnlargeButton}
+                            class="absolute top-0 right-0 p-5 ease-in-out hover:scale-125"
+                            onClick={(event) => (enlargeType() ? toggleEnlargeIFrame() : toggleEnlargeImage(event))}>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             x="0px"
+                             y="0px"
+                             width="35"
+                             height="35"
+                             fill="white"
                              viewBox="0 0 50 50">
                             <path
                                 d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
